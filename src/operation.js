428d144a8167b2a3d1c7dca5903cb1b4e07db8b1
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-07 15:11:42 +0700

    update: display

--- src/operation.js
+++ src/operation.js
@@ -8,3 +8,23 @@ export function getEuclidianDistance (coordinate1, coordinate2) {
     const distance = Math.sqrt(deltax * deltax + deltay * deltay);
     return distance;
 }
+
+export function displayMatrix (mapAdjMatrix) {
+    console.log(" ================= Matrix of Adjacency =================== ");
+    const matrix = mapAdjMatrix;
+    for (let i = 0; i < matrix[1].length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            process.stdout.write(matrix[i][j].toString() + " ");
+        }
+        console.log();
+    }
+    console.log();
+}
+
+export function displayCoordinate (array) {
+    console.log(" ================= Array of Coordinates =================== ");
+    for (let i = 0; i < array.length; i++) {
+        console.log(i + ": " + array[i].x.toString() + ", " + array[i].y.toString());
+    }
+    console.log();
+}
