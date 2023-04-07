52f16a5fa17f8efd39f28e37f6f3dbbd8d575736
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-04-07 18:33:05 +0700

    Add input from file and validation

---
+++ src/input.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+
+// const fs = require('fs');
+export function readFile(filePath) {
+    const result = [];
+    const contents = fs.readFileSync(filePath, 'utf-8');
+    // Read per line
+    const lines = contents.trim().split('\n');
+    // Split lines
+    for (let i = 0; i < lines.length; i++) {
+        const splitted = lines[i].trim().split(/\s+/).map(Number);
+        result.push(splitted);
+    }
+
+    return result;
+}
+
+export function validMap(adjMatrix) {
+    for (let i = 0; i < adjMatrix.length; i++) {
+        for (let j = 0; j < adjMatrix[0].length; j++) {
+            if (Number.isNaN(adjMatrix[i][j]) || adjMatrix[i][j] < -1) {
+                console.log("Invalid element in (", i, ",", j, ")");
+                console.log("Element must be integer. The minimum value of -1 denotes a non-neighboring node");
+                return false;
+            }
+        }
+    }
+    if (adjMatrix.length == 0) {
+        console.log("Empty Adjacency Matrix. Try Again!");
+        return false;
+    }
+    if (adjMatrix.length != adjMatrix[0].length) {
+        console.log("Invalid Map AdjacencyMatrix. Try Again!");
+        console.log("Adjacency Matrix must be square and separated by spaces between their columns.");
+        return false;
+    }
+    return true;
+}
+
+export function validNode(inputNode, adjMatrix) {
+    if (inputNode < 0 || inputNode >= adjMatrix.length) {
+        console.log("Invalid input! Choose node 0 -", adjMatrix[0].length - 1);
+        return false;
+    } else return true;
+}
--- src/mainProgram.js
+++ src/mainProgram.js
@@ -1,6 +1,9 @@
 import { PriorityQueue } from "./PriorityQueue.js";
 import { UCS, distance } from "./UCS.js"
 import { Astar } from "./Astar.js";
+import { readFile, validMap, validNode } from "./input.js";
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
 
 // Main Program
 console.log("Entering program...\n");
@@ -19,7 +22,7 @@ const matrixOfSTD = [
 ]
 
 // jarak aslinya ke tetangga pake yang bawah
-
+/*
 const mapAdjMatrix = [
     [0, 10, 2, -1, 5, -1],
     [10, 0, -1, 3, -1, 6],
@@ -28,9 +31,35 @@ const mapAdjMatrix = [
     [5, -1, -1, 4, 0, 10],
     [-1, 6, -1, -1, 10, 0]
 ]
+*/
 
-const startNode = 0;
-const goalNode = 5;
+// Input file
+//const prompt = require("prompt-sync")();
+
+var fileName = prompt('Input file name : ');
+var mapAdjMatrix;
+
+try {
+    mapAdjMatrix = readFile("test/" + fileName + ".txt");
+} catch (err) {
+    console.log("File not found");
+    console.log("Make sure the .txt file is stored in the test folder");
+}
+
+while (!validMap(mapAdjMatrix)) {
+    fileName = prompt("Input file name : ");
+    mapAdjMatrix = readFile("test/" + fileName + ".txt");
+}
+
+console.log("There is", mapAdjMatrix.length, "node : 0 -", mapAdjMatrix[0].length - 1);
+var startNode = prompt("Input start node : ");
+while (!validNode(startNode, mapAdjMatrix)) {
+    startNode = prompt("Input start node : ");
+}
+var goalNode = prompt("Input goal node : ");
+while (!validNode(goalNode, mapAdjMatrix)) {
+    goalNode = prompt("Input goal node : ");
+}
 
 const resultPath = UCS(mapAdjMatrix, startNode, goalNode);
 const path = Astar(mapAdjMatrix, arrayOfCoordinates, startNode, goalNode);
@@ -40,11 +69,9 @@ else {
     const resultDistance = distance(resultPath, mapAdjMatrix);
     console.log("Path found with total distance " + resultDistance);
     console.log("Shortest path : " + Array.from(resultPath.values()));
-    console.log("\n");
 }
 
-console.log(" =================== A STAR ===================== ");
+console.log("\n=================== A STAR ===================== ");
 const AsDistance = distance(path, mapAdjMatrix);
 console.log("Path found with total distance " + AsDistance);
 console.log("Shortest path : " + path);
-console.log("\n");
