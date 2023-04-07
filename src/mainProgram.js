119d2341fe79b9fadda4a7bbd4f7413683c3dc91
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-07 14:54:57 +0700

    update: deleting astar main

--- src/mainProgram.js
+++ src/mainProgram.js
@@ -8,16 +8,6 @@ console.log("Entering program...\n");
 // Array to find straight line distance
 const arrayOfCoordinates = [{ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }, { x: 5, y: 5 }, { x: 6, y: 6 }];
 
-// ceritanya udah diitung (jarak std)
-const matrixOfSTD = [
-    [0, 1, 1, 1, 1, 1],
-    [2, 0, 1, 1, 1, 1],
-    [3, 1, 0, 1, 1, 1],
-    [5, 1, 1, 0, 1, 1],
-    [6, 1, 1, 1, 0, 1],
-    [3, 1, 1, 1, 1, 0],
-]
-
 // jarak aslinya ke tetangga pake yang bawah
 
 const mapAdjMatrix = [
