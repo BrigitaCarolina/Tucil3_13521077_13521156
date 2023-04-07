7f4aa24ae780e75431ae67cf7026b3f031ae2abc
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-07 17:18:46 +0700

    feat: adding map

--- src/map.js
+++ src/map.js
@@ -1,28 +1,7 @@
-// Define the locations as vertices
-var nodes = new vis.DataSet([
-{id: 1, label: 'San Francisco'},
-{id: 2, label: 'Los Angeles'},
-{id: 3, label: 'Las Vegas'},
-{id: 4, label: 'Seattle'},
-{id: 5, label: 'New York'},
-{id: 6, label: 'Chicago'}
-]);
-
-// Define the paths between locations as edges
-var edges = new vis.DataSet([
-{from: 1, to: 2},
-{from: 1, to: 3},
-{from: 1, to: 4},
-{from: 5, to: 6},
-{from: 5, to: 1},
-{from: 6, to: 2}
-]);
-
-// Create the graph
-var container = document.getElementById('map');
-var data = {
-nodes: nodes,
-edges: edges
-};
-var options = {};
-var network = new vis.Network(container, data, options);
+function initMap() {
+    var location = {lat: -25.363, lng: 131.044}
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom:8,
+      center:location
+    })
+  }
